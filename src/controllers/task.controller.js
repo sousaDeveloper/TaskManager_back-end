69c4de2b24492513e35dfe5d4db9fd6ce6a03365
Matheus Sousa <devlpsousa@gmail.com>
2024-02-15 14:30:36 -0300

    feat: move get, getById, create, update and delete tasks endpoint to a controller

---
+++ src/controllers/task.controller.js
@@ -0,0 +1,93 @@
+const TaskModel = require("../models/task.model");
+
+class TaskController {
+  constructor(req, res) {
+    this.req = req;
+    this.res = res;
+  }
+
+  async getAll() {
+    try {
+      const tasks = await TaskModel.find({});
+
+      this.res.status(200).send(tasks);
+    } catch (error) {
+      this.res.status(500).send(message.error);
+    }
+  }
+
+  async getById() {
+    try {
+      const taskId = this.req.params.id;
+
+      const task = await TaskModel.findById(taskId);
+
+      if (!task) {
+        this.res.status(404).send("Essa tarefa não foi encontrada.");
+      }
+
+      return this.res.status(200).send(task);
+    } catch (error) {
+      this.res.status(500).send(error.message);
+    }
+  }
+
+  async create() {
+    try {
+      const newTask = new TaskModel(this.req.body);
+
+      await newTask.save();
+
+      this.res.status(201).send(newTask);
+    } catch (error) {
+      this.res.status(500).send(message.error);
+    }
+  }
+
+  async update() {
+    try {
+      const taskId = this.req.params.id;
+      const taskData = this.req.body;
+
+      const taskToUpdate = await TaskModel.findById(taskId);
+
+      const allowedUpdates = ["isCompleted"];
+      const requestedUpdates = Object.keys(taskData);
+
+      for (const update of requestedUpdates) {
+        if (allowedUpdates.includes(update)) {
+          taskToUpdate[update] = taskData[update];
+        } else {
+          return this.res
+            .status(500)
+            .send("Um ou mais campos inseridos não são editáveis.");
+        }
+      }
+
+      await taskToUpdate.save();
+      return this.res.status(200).send(taskToUpdate);
+    } catch (error) {
+      this.res.status(500).send(error.message);
+    }
+  }
+
+  async delete() {
+    try {
+      const taskId = this.req.params.id;
+
+      const taskToDelete = await TaskModel.findById(taskId);
+
+      if (!taskToDelete) {
+        return this.res.status(404).send("Essa tarefa não foi encontrada.");
+      }
+
+      const deletedTask = await TaskModel.findByIdAndDelete(taskId);
+
+      this.res.status(200).send(deletedTask);
+    } catch (error) {
+      this.res.status(500).send(message.error);
+    }
+  }
+}
+
+module.exports = TaskController;
--- src/routes/task.routes.js
+++ src/routes/task.routes.js
@@ -1,89 +1,27 @@
-const TaskModel = require("../models/task.model");
 const express = require("express");
+
+const TaskController = require("../controllers/task.controller");
 
 const router = express.Router();
 
 router.get("/", async (req, res) => {
-  try {
-    const tasks = await TaskModel.find({});
-
-    res.status(200).send(tasks);
-  } catch (error) {
-    res.status(500).send(message.error);
-  }
+  return new TaskController(req, res).getAll();
 });
 
 router.get("/:id", async (req, res) => {
-  try {
-    const taskId = req.params.id;
-
-    const task = await TaskModel.findById(taskId);
-
-    if (!task) {
-      res.status(404).send("Essa tarefa não foi encontrada.");
-    }
-
-    return res.status(200).send(task);
-  } catch (error) {
-    res.status(500).send(error.message);
-  }
+  return new TaskController(req, res).getById();
 });
 
 router.post("/", async (req, res) => {
-  try {
-    const newTask = new TaskModel(req.body);
-
-    await newTask.save();
-
-    res.status(201).send(newTask);
-  } catch (error) {
-    res.status(500).send(message.error);
-  }
+  return new TaskController(req, res).create();
 });
 
 router.patch("/:id", async (req, res) => {
-  try {
-    const taskId = req.params.id;
-    const taskData = req.body;
-
-    const taskToUpdate = await TaskModel.findById(taskId);
-
-    const allowedUpdates = ["isCompleted"];
-    const requestedUpdates = Object.keys(taskData);
-
-    for (update of requestedUpdates) {
-      if (allowedUpdates.includes(update)) {
-        taskToUpdate[update] = taskData[update];
-      } else {
-        return res
-          .status(500)
-          .send("Um ou mais campos inseridos não são editáveis.");
-      }
-    }
-
-    await taskToUpdate.save();
-    return res.status(200).send(taskToUpdate);
-  } catch (error) {
-    res.status(500).send(error.message);
-  }
+  return new TaskController(req, res).update();
 });
 
 router.delete("/:id", async (req, res) => {
-  try {
-    const taskId = req.params.id;
-
-    const taskToDelete = await TaskModel.findById(taskId);
-
-    if (!taskToDelete) {
-      return res.status(404).send("Essa tarefa não foi encontrada.");
-    }
-
-    const deletedTask = await TaskModel.findByIdAndDelete(taskId);
-
-    res.status(200).send(deletedTask);
-  } catch (error) {
-    res.status(500).send(message.error);
-  }
+  return new TaskController(req, res).delete();
 });
 
 module.exports = router;
